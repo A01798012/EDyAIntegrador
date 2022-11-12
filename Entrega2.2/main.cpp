f09762809876cbfb3e96fbafd04ced8309adb68d
a01798102 <arturomontes481@gmail.com>
2022-11-11 19:44:18 -0600

    Adicion de loop para que el usuario haga el numero de consultas que desee

--- Entrega2.2/main.cpp
+++ Entrega2.2/main.cpp
@@ -20,20 +20,26 @@ int main() {
     bitacora.almacenarFallas();
 
     // PUNTOS 3 y 4:
-    string fallaPedida;
-    cout << "Los tipos de fallas son:\n\n";
-    bitacora.imprimirFallas();
-    cout << "Ingrese el tipo de falla a buscar: ";
-    getline(cin, fallaPedida);
-    cout << endl;
-    bitacora.imprimirYGuardarPorFalla(fallaPedida, archivoConsultaPorFalla);
+    while(1){
+        string fallaPedida;
+        cout << "Los tipos de fallas son:\n\n";
+        bitacora.imprimirFallas();
+        cout << "Ingrese el tipo de falla a buscar(digite -1 para salir): ";
+        getline(cin, fallaPedida);
+        if(fallaPedida == "-1") break;
+        cout << endl;
+        bitacora.imprimirYGuardarPorFalla(fallaPedida, archivoConsultaPorFalla);
+    }
 
     // PUNTOS 5 Y 6:
-    string horaPedida;
-    cout << endl << "Ingrese la hora a buscar (01,13,21,etc): ";
-    cin >> horaPedida;
-    cout << endl;
-    bitacora.imprimirYGuardarPorHora(horaPedida, archivoConsultaPorHora);
+    while(1){
+        string horaPedida;
+        cout << endl << "Ingrese la hora a buscar (01,13,21, etc o digite -1 para salir): ";
+        cin >> horaPedida;
+        if(horaPedida == "-1") break;
+        cout << endl;
+        bitacora.imprimirYGuardarPorHora(horaPedida, archivoConsultaPorHora);
+    }
 
     return 0;
 }
