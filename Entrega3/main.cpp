76f3e92bbd3428325fe976a3ce15b69a998fcff2
a01798102 <arturomontes481@gmail.com>
2022-11-15 16:36:01 -0600

    cambios a main con todos los casos de prueba

--- Entrega3/main.cpp
+++ Entrega3/main.cpp
@@ -2,7 +2,19 @@
 
 
 int main(){
-    BST<int> * arbol = new BST<int>();
+    BST<int> * arbol1 = new BST<int>();
+    BST<int> * arbol2 = new BST<int>();
+    BST<int> * arbol3 = new BST<int>();
+    BST<int> * arbol4 = new BST<int>();
+    int a1[7] = {21, 13, 10, 18, 33, 25, 40};
+    int a2[15] =  {3, 14, 21, 22, 25, 27, 30, 33, 43, 47, 59, 60, 63, 78, 89};
+    int a3[15] = {-48, -45, -41, -40, -35, -15, -8, -5, 3, 4, 12, 14, 17, 25, 38};
+    int a4[8] = {29, 34, 42, 46, 47, 50, 53, 60};
+
+    for(int i = 0; i <  7; i++) arbol1->agregarNodo(a1[i]);
+    for(int i = 0; i <  15; i++) arbol2->agregarNodo(a2[i]);
+    for(int i = 0; i <  15; i++) arbol3->agregarNodo(a3[i]);
+    for(int i = 0; i <  8; i++) arbol4->agregarNodo(a4[i]);
     /*arbol->agregarNodo(21);
     arbol->agregarNodo(13);
     arbol->agregarNodo(10);
@@ -15,26 +27,19 @@ int main(){
     arbol->imprimirInOrder();
     arbol->imprimirPostOrder();
     */
-    arbol->agregarNodo(21);
-    arbol->agregarNodo(13);
-    arbol->agregarNodo(10);
-    arbol->agregarNodo(18);
-    arbol->agregarNodo(33);
-    arbol->agregarNodo(25);
-    arbol->agregarNodo(40);
-    cout << "Altura: " << arbol ->height() << endl;
-    arbol->agregarNodo(44);
-    cout << "Altura: " << arbol ->height() << endl;
-    arbol->imprimirPreOrder();
-    arbol->imprimirInOrder();
-    arbol->imprimirPostOrder();
-    arbol->eliminarNodo(18);
-    arbol->imprimirInOrder();
-    arbol->eliminarNodo(13);
-    arbol->imprimirInOrder();
-    arbol->eliminarNodo(33);
-    arbol->imprimirInOrder();
-    arbol->eliminarNodo(17);
+    cout << "Altura: " << arbol1 ->height() << endl;
+    arbol1->agregarNodo(44);
+    cout << "Altura: " << arbol1 ->height() << endl;
+    arbol1->imprimirPreOrder();
+    arbol1->imprimirInOrder();
+    arbol1->imprimirPostOrder();
+    arbol1->eliminarNodo(18);
+    arbol1->imprimirInOrder();
+    arbol1->eliminarNodo(13);
+    arbol1->imprimirInOrder();
+    arbol1->eliminarNodo(33);
+    arbol1->imprimirInOrder();
+    arbol1->eliminarNodo(17);
 
 
    
